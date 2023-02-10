bed4c70f69eb6525652d5847de1e2f86609186b7
ricardo <rwmsousa@gmail.com>
2023-02-10 17:50:10 -0300

    feat: test acc toobar

--- src/pages/components/AccToolBar/index.tsx
+++ src/pages/components/AccToolBar/index.tsx
@@ -1,7 +1,11 @@
+import { useEffect } from 'react';
 import Script from 'next/script'
 import MicAccessTool from '../AccToolBar/app/js/common';
 
 export default function AccToolBar (){
-  new MicAccessTool({});
+  
+  useEffect(()=>{
+    new MicAccessTool({});
+  },[])
   return <Script src="./acctoolbar.min.js" />  
 }
