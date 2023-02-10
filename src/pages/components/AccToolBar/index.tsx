38323c9091ea908aa412d999815cee6a8f5d01ee
ricardo <rwmsousa@gmail.com>
2023-02-10 17:52:08 -0300

    feat: test acc toobar

--- src/pages/components/AccToolBar/index.tsx
+++ src/pages/components/AccToolBar/index.tsx
@@ -2,10 +2,14 @@ import { useEffect } from 'react';
 import Script from 'next/script'
 import MicAccessTool from '../AccToolBar/app/js/common';
 
-export default function AccToolBar (){
-  
-  useEffect(()=>{
-    new MicAccessTool({});
-  },[])
-  return <Script src="./acctoolbar.min.js" />  
+export default function AccToolBar() {
+  const page = window.document
+
+  useEffect(() => {
+    if (typeof page !== 'undefined') {
+      new MicAccessTool({});
+    }
+  }, [page])
+
+  return <Script src="./acctoolbar.min.js" />
 }
