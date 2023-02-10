6168ed4ec5f94545c0ef497ccac0963de18675d6
ricardo <rwmsousa@gmail.com>
2023-02-10 17:48:36 -0300

    feat: test acc toobar

--- src/pages/components/AccToolBar/index.tsx
+++ src/pages/components/AccToolBar/index.tsx
@@ -2,16 +2,6 @@ import Script from 'next/script'
 import MicAccessTool from '../AccToolBar/app/js/common';
 
 export default function AccToolBar (){
-  <>
-    <Script src="./acctoolbar.min.js" />
-
-      window.onload = function() {
-        window.micAccessTool = new MicAccessTool({
-          link: 'http://your-awesome-website.com/your-accessibility-declaration.pdf',
-          contact: 'mailto:your-mail@your-awesome-website.com',
-          buttonPosition: 'right', // default is 'left'
-          forceLang: 'ru-RU' // default is 'en' may be 'he-IL', 'ru-RU', or 'fr_FR'
-        })
-      }
-    </>
+  new MicAccessTool({});
+  return <Script src="./acctoolbar.min.js" />  
 }
