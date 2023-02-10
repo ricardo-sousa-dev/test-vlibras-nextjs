bb2ebae91c2f1d85c3005b51b68586b24e1c79f2
ricardo <rwmsousa@gmail.com>
2023-02-10 13:34:53 -0300

    refactor: add header access-controll default

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://repository-dth.vlibras.gov.br/api/signs" },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
