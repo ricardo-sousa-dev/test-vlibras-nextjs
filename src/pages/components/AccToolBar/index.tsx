6bdf1dce2b630e58c9babed9cc21a446a657c923
ricardo <rwmsousa@gmail.com>
2023-02-15 09:54:33 -0300

    refactor: improve control stage contrast

--- src/pages/components/AccToolBar/index.tsx
+++ src/pages/components/AccToolBar/index.tsx
@@ -1,14 +1,7 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './accToolBar.module.scss';
 
 export default function AccToolBar() {
-
-  const primary = '#00bf98';
-  const secondary = '#353e52';
-  const light = '#f1f1f1';
-  const black = '#000000';
-
-  const [ contrast, setContrast ] = useState(1);
 
   let tagsH1: HTMLCollectionOf<HTMLElement>;
   let tagsH2: HTMLCollectionOf<HTMLElement>;
@@ -20,6 +13,33 @@ export default function AccToolBar() {
   let tagsButton: HTMLCollectionOf<HTMLElement>;
   let storageContrast: string | null;
   let arrayTags: HTMLCollectionOf<HTMLElement>[];
+
+  const [ contrast, setContrast ] = useState(1);
+
+  // tag[ t ].removeAttribute('style');
+  // document.getElementsByTagName('body')[ 0 ].style.removeProperty('background');
+
+  const modifyingContrastClear = () => {
+    for (let i = 0; i < arrayTags.length; i++) {
+      const tag = arrayTags[ i ];
+      for (let t = 0; t < tag.length; t++) {
+        tag[ t ].style.removeProperty('color');
+        tag[ t ].style.removeProperty('background');
+      }
+    }
+  }
+
+  const modifyingContrast2 = () => {
+    for (let i = 0; i < arrayTags.length; i++) {
+      const tag = arrayTags[ i ];
+      for (let t = 0; t < tag.length; t++) {
+        tag[ t ].setAttribute('style', 'color: blue');
+        if (tag[ t ].tagName === 'BUTTON') {
+          tag[ t ].setAttribute('style', 'background: blue; color: white');
+        }
+      }
+    }
+  }
 
   if (typeof window !== 'undefined') {
 
@@ -33,65 +53,43 @@ export default function AccToolBar() {
     tagsButton = document.getElementsByTagName('button');
     storageContrast = localStorage.getItem('storageContrast') || null;
     arrayTags = [ tagsH1, tagsH2, tagsH3, tagsH4, tagsH5, tagsH6, tagsP, tagsButton ];
+
   }
 
-  // tag[ t ].style.removeProperty('color');
-  // document.getElementsByTagName('body')[ 0 ].style.removeProperty('background');
-
-  const modifyingContrastClear = () => {
-    for (let i = 0; i < arrayTags.length; i++) {
-      const tag = arrayTags[ i ];
-      for (let t = 0; t < tag.length; t++) {
-        tag[ t ].removeAttribute('style');
-      }
-    }
-    localStorage.setItem('storageContrast', JSON.stringify(2))
-  }
-
-  const modifyingContrast2 = () => {
-    for (let i = 0; i < arrayTags.length; i++) {
-      const tag = arrayTags[ i ];
-      for (let t = 0; t < tag.length; t++) {
-        tag[ t ].setAttribute('style', 'color: blue');
-        if (tag[ t ].tagName === 'BUTTON') {
-          tag[ t ].setAttribute('style', 'background: blue; color: white');
-        }
-      }
-    }
-    localStorage.setItem('storageContrast', JSON.stringify(1))
-  }
-
-  const changeContrast = useCallback(() => {
-    console.log('CONTRAST', contrast)
-    if (contrast < 2) {
-      setContrast(contrast + 1);
-
-    } else {
-      setContrast(1);
-    }
-
-    switch (contrast) {
-      case 1:
-        modifyingContrast2();
-        break;
-      case 2:
-        modifyingContrastClear();
-        break;
-      default:
-        break;
-    }
-  }, [ contrast ]);
-
-  useEffect(() => {
+  useEffect(()=> {
     if (!storageContrast) {
       localStorage.setItem('storageContrast', JSON.stringify(1))
-    }
+    } else {
 
-    if (storageContrast && JSON.parse(storageContrast)) {
-      setContrast(JSON.parse(storageContrast))
+      switch (JSON.parse(storageContrast)) {
+        case 1:
+          console.log('ENTROU');
+          setContrast(1)
+          modifyingContrastClear();
+          break;
+        case 2:
+          setContrast(2)
+          modifyingContrast2();
+          break;
+      }
     }
+  },[])
 
-  }, [])
+
+  const changeContrast = () => {
+   
+    if (contrast == 1) {
+      setContrast(2);
+      modifyingContrast2();
+      localStorage.setItem('storageContrast', JSON.stringify(2))
+      
+    } else if (contrast == 2) {
+      setContrast(1);
+      modifyingContrastClear();
+      localStorage.setItem('storageContrast', JSON.stringify(1))
+    }
+  };
+
 
   return (
     <div className={ style.accToolBar }>
