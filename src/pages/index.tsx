b857e0ba34d77b0aa69949d22c054c25458d61f1
ricardo <rwmsousa@gmail.com>
2023-02-24 14:24:05 -0300

    refactor: test VLibras in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,25 @@
 import styles from '@/styles/Home.module.scss'
 import VLibras from '../components/VLibras';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [ joke, setJoke ] = useState('');
+  // const [ joke, setJoke ] = useState('');
 
-  useEffect(() => {
-    axios.get('https://geek-jokes.sameerkumar.website/api?format=json').then((response) => {
-      setJoke(response.data.joke);
-    });
-  }, [])
+  // useEffect(() => {
+  //   axios.get('https://geek-jokes.sameerkumar.website/api?format=json').then((response) => {
+  //     setJoke(response.data.joke);
+  //   });
+  // }, [])
 
   return (
     <>
       { process.env.NODE_ENV === "production" && <VLibras forceOnload /> }
 
       <div className={ styles.section }>
-        <h1>Accessibility Testing with NextJS</h1>
-        <h1>Geek Jokes</h1>
-        <h2>Test in English:</h2>
-        <p> { joke }</p>
+        <h1>Teste de ferramentas de acessibilidade - NextJS</h1>
+        <h1>Teste h1</h1>
+        <h2>Teste h2</h2>
         <p>Libras é a sigla da Língua Brasileira de Sinais, uma língua de modalidade gestual-visual onde é possível se comunicar através de gestos, expressões faciais e corporais. É reconhecida como meio legal de comunicação e expressão desde 24 de Abril de 2002, através da Lei nº 10.436. A Libras é muito utilizada na comunicação com pessoas surdas, sendo, portanto, uma importante ferramenta de inclusão social.</p>
         <button>Button</button>
         <div>Div1
