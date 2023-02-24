716106e61c664aa7d907d9067801e419d4b6e7af
ricardo <rwmsousa@gmail.com>
2023-02-24 14:19:12 -0300

    refactor: test VLibras in app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import type { AppProps } from 'next/app'
 // import AccToolBar from '../components/AccToolBar'
-// import VLibras from '../components/VLibras';
-// import VLibras from 'vlibras-nextjs';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* { process.env.NODE_ENV === "production" && <VLibras forceOnload /> } */}
       {/* <AccToolBar /> */ }
       <Component { ...pageProps } />
-      {/* <VLibras forceOnload={true} /> */}
     </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import VLibras from 'vlibras-nextjs';
 
 export default function Document() {
   return (
@@ -8,8 +7,6 @@ export default function Document() {
         <link rel="shortcut icon" href="/a11y-symbol.webp" />
       </Head>
       <body>
-        { process.env.NODE_ENV === "production" && <VLibras forceOnload /> }
-
         <Main />
         <NextScript />
       </body>
