f23a7eb92b95a5efa060417503dbc54dfb999ef2
ricardo <rwmsousa@gmail.com>
2023-02-27 13:45:35 -0300

    feat: last version Libras toll

--- src/components/VLibras/index.tsx
+++ src/components/VLibras/index.tsx
@@ -1,48 +1,37 @@
-import React, { EffectCallback, useEffect } from "react";
+import React, { EffectCallback, useEffect } from 'react';
 
-type ExpectedReadyState =
-  | ReadonlyArray<DocumentReadyState>
-  | DocumentReadyState
-  | undefined;
+type ExpectedReadyState = ReadonlyArray<DocumentReadyState> | DocumentReadyState | undefined;
 
 const isReadyStateMatch = (expected?: ExpectedReadyState): boolean => {
   if (!expected) {
     return true;
   }
-  if (typeof expected === "string" && document.readyState === expected) {
+  if (typeof expected === 'string' && document.readyState === expected) {
     return true;
   }
+
   return expected.indexOf(document.readyState) !== -1;
 };
+/* eslint-disable */
+type useReadyStateEffect = (effect: EffectCallback, deps?: any[], onState?: ExpectedReadyState) => void;
 
-type useReadyStateEffect = (
-  effect: EffectCallback,
-  deps?: any[],
-  onState?: ExpectedReadyState
-) => void;
-
-const useReadyStateEffect: useReadyStateEffect = (
-  effect,
-  deps = [],
-  onState = "complete"
-): void => {
+const useReadyStateEffect: useReadyStateEffect = (effect, deps = [], onState = 'complete'): void => {
   useEffect(() => {
-    const destructors: Array<() => void> = [
-      () => document.removeEventListener("readystatechange", listener),
-    ];
+    const destructors: Array<() => void> = [ () => document.removeEventListener('readystatechange', listener) ];
 
     const listener = () => {
       if (!isReadyStateMatch(onState)) {
         return;
       }
       const destructor = effect();
+
       if (destructor) {
         destructors.push(destructor);
       }
     };
 
     listener();
-    document.addEventListener("readystatechange", listener);
+    document.addEventListener('readystatechange', listener);
 
     return () => destructors.forEach((d) => d());
   }, deps);
@@ -52,36 +41,46 @@ type Props = {
   forceOnload?: boolean;
 };
 
-function VLibras({ forceOnload }: Props): JSX.Element {
+function Libras({ forceOnload }: Props): JSX.Element {
   useReadyStateEffect(
     () => {
-      const script = document.createElement("script");
-      script.src = "https://vlibras.gov.br/app/vlibras-plugin.js";
+      const script = document.createElement('script');
+
+      script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
       script.async = true;
-      const widgetUrl = `https://vlibras.gov.br/app`;
+      const widgetUrl = 'https://vlibras.gov.br/app';
+
       script.onload = () => {
         // @ts-ignore
+
         new window.VLibras.Widget(widgetUrl);
         if (forceOnload) {
           // @ts-ignore
+
           window.onload();
         }
       };
       document.head.appendChild(script);
     },
     [ forceOnload ],
-    "complete"
+    'complete',
   );
 
   return (
-    // @ts-ignore
-    <div vw="true" className="enabled">
-      <div vw-access-button="true" className="active" />
-      <div vw-plugin-wrapper="true">
-        <div className="vw-plugin-top-wrapper" />
+    <div
+      // @ts-ignore
+      vw='true'
+      className='enabled'>
+      <div
+        vw-access-button='true'
+        className='active'
+      />
+      <div vw-plugin-wrapper='true'>
+        <div className='vw-plugin-top-wrapper' />
       </div>
     </div>
   );
 }
 
-export default VLibras;
+export default Libras;
+// https://github.com/vandsonfalcao/vlibras-nextjs
