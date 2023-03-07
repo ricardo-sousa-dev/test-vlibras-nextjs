b704ae9f795f9a9a74eb16a2271f28ab1484554a
ricardo <rwmsousa@gmail.com>
2023-03-07 17:49:51 -0300

    refactor: remove import unsead

--- src/components/AccToolbar/index.tsx
+++ src/components/AccToolbar/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import Script from 'next/script';
 
 import FloatingIcon from './FloatingIcon';
 import Toolbar from './Toolbar';
