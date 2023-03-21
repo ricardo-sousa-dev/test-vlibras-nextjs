11aa3ddfaa9595a758c3188764cea9f9e4c66cb9
ricardo <rwmsousa@gmail.com>
2023-03-21 17:20:36 -0300

    feat: test libras button

--- src/components/AccToolbar/ButtonLibras/index.tsx
+++ src/components/AccToolbar/ButtonLibras/index.tsx
@@ -2,30 +2,23 @@ import ToolbarButton from '../ToolbarButton';
 
 /*eslint-disable */
 export default function Libras() {
-  let widgetLibras: Element;
-
-  if (typeof window != 'undefined') {
-    for (let index = 0; index < document.getElementsByTagName('DIV').length; index++) {
-      const element = document.getElementsByTagName('DIV')[ index ];
-      if (element.getAttribute('vw-plugin-wrapper')) {
-        widgetLibras = element;
-        if (localStorage.getItem('storageLibras') == '2') {
-          widgetLibras.setAttribute('class', 'active')
-        }
-      }
-    }
-  }
 
   const setLibras = () => {
-    if (localStorage.getItem('storageLibras') == '1') {
-      widgetLibras.setAttribute('class', 'active')
-      localStorage.setItem('storageLibras', JSON.stringify(2))
-
-    } else if (localStorage.getItem('storageLibras') == '2') {
-      widgetLibras.removeAttribute('class')
-      localStorage.setItem('storageLibras', JSON.stringify(1))
+    for (let index = 0; index < document.getElementsByTagName('IMG').length; index++) {
+      const element: HTMLElement = document.getElementsByTagName('IMG')[ index ] as HTMLElement;
+      if (element.getAttribute('class') === 'access-button') {
+        element.click()
+      }
     }
+
+    // if (localStorage.getItem('storageLibras') == '1') {
+    //   localStorage.setItem('storageLibras', JSON.stringify(2))
+
+    // } else if (localStorage.getItem('storageLibras') == '2') {
+    //   localStorage.setItem('storageLibras', JSON.stringify(1))
+    // }
   }
+
 
   return (
     <ToolbarButton
