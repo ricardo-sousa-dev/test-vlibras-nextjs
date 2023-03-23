a4d0e11265150b957fa95cf2edd0e07bed21424d
ricardo <rwmsousa@gmail.com>
2023-03-23 15:24:50 -0300

    refactor: test libras button

--- src/components/AccToolbar/ButtonContrast/index.tsx
+++ src/components/AccToolbar/ButtonContrast/index.tsx
@@ -88,9 +88,7 @@ export default function ContrastButton() {
           
           if (!notChangeIfAttributeLibras(tag)) {
             tag.style.background = 'white'
-          }else{
-            tag.style.color = 'white';
-        }
+          }
 
           if (tag.tagName === 'BUTTON') {
             tag.style.background = 'blue !important';
