32cf1e54a249f019785520912bbdb687ef2819c2
ricardo <rwmsousa@gmail.com>
2023-03-23 16:30:02 -0300

    refactor: test libras button

--- src/components/AccToolbar/ButtonContrast/index.tsx
+++ src/components/AccToolbar/ButtonContrast/index.tsx
@@ -132,11 +132,10 @@ export default function ContrastButton() {
           }
           setTimeout(() => {
             document.getElementsByClassName('vpw-mes')[ 0 ].setAttribute('style', 'color: white')
-            if (!notChangeStyle(tag)) {
+            if (notChangeStyle(tag)) {
               tag.style.removeProperty('background')
               tag.style.removeProperty('border')
             }
-            
           }, 5000)
           break;
         case 3:
