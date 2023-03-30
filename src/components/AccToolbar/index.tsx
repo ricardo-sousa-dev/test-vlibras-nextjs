346253682b9ce08798980b2c8437cd73c3da03d2
ricardo <rwmsousa@gmail.com>
2023-03-30 11:12:17 -0300

    refactor: init load libras hiddened

--- src/components/AccToolbar/index.tsx
+++ src/components/AccToolbar/index.tsx
@@ -10,19 +10,31 @@ export default function AccToolbar() {
   const [ showToolbar, setShowToolbar ] = useState(false);
 
   useEffect(() => {
-   Head()
-
-    for (let index = 0; index < document.getElementsByTagName('DIV').length; index++) {
-      const element: HTMLElement = document.getElementsByTagName('DIV')[ index ] as HTMLElement;
-      if (element.getAttribute('vw-access-button')){
-        element.setAttribute('style','display:none');
-        element.click();
-      }
-    }
+    Head()
     const storageContrast = localStorage.getItem('storageContrast')
     const storageTextStyle = localStorage.getItem('storageTextStyle')
     const storageZoomPage = localStorage.getItem('storageZoomPage')
     const storageLibras = localStorage.getItem('storageLibras')
+
+    for (let index = 0; index < document.getElementsByTagName('DIV').length; index++) {
+      const element: HTMLElement = document.getElementsByTagName('DIV')[ index ] as HTMLElement;
+      if (element.getAttribute('vw-access-button')) element.setAttribute('style', 'display:none');
+    }
+
+    for (let index = 0; index < document.getElementsByTagName('IMG').length; index++) {
+      const element: HTMLElement = document.getElementsByTagName('IMG')[ index ] as HTMLElement;
+      if (element.getAttribute('class') === 'access-button') {
+        if (storageLibras == '2') element.click()
+      }
+    }
+  
+    for (let index = 0; index < document.getElementsByTagName('div').length; index++) {
+      const element: HTMLElement = document.getElementsByTagName('div')[ index ] as HTMLElement;
+      if (element.getAttribute('class') === 'vw-plugin-wrapper') {
+        // if (storageLibras == '2') element.click()
+        element.style.display = 'none';
+      }
+    }
 
     if (!storageContrast) localStorage.setItem('storageContrast', JSON.stringify(1))
     if (!storageTextStyle) localStorage.setItem('storageTextStyle', JSON.stringify(1))
@@ -33,17 +45,17 @@ export default function AccToolbar() {
       || storageTextStyle && storageTextStyle !== '1'
       || storageZoomPage && storageZoomPage !== '1'
       || storageLibras && storageLibras !== '1'
-    // ) setTimeout(() => setShowToolbar(true), 2000);
+      // ) setTimeout(() => setShowToolbar(true), 2000);
     ) setShowToolbar(true);
 
-    setTimeout(() => {
-      for (let index = 0; index < document.getElementsByTagName('IMG').length; index++) {
-        const element: HTMLElement = document.getElementsByTagName('IMG')[ index ] as HTMLElement;
-        if (element.getAttribute('class') === 'access-button') {
-          if (storageLibras == '2') element.click()
-        }
-      }
-    }, 2000);
+  //   setTimeout(() => {
+  //     for (let index = 0; index < document.getElementsByTagName('IMG').length; index++) {
+  //       const element: HTMLElement = document.getElementsByTagName('IMG')[ index ] as HTMLElement;
+  //       if (element.getAttribute('class') === 'access-button') {
+  //         if (storageLibras == '2') element.click()
+  //       }
+  //     }
+  //   }, 2000);
   }, []);
 
   const closeToolbar = () => {
