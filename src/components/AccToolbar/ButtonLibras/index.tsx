b76013ff9100d468a618c1d432e665c286d8fefc
ricardo <rwmsousa@gmail.com>
2023-03-30 14:42:50 -0300

    refactor: init load libras hiddened

--- src/components/AccToolbar/ButtonLibras/index.tsx
+++ src/components/AccToolbar/ButtonLibras/index.tsx
@@ -5,24 +5,26 @@ import ToolbarButton from '../ToolbarButton';
 export default function Libras() {
 
   const storageLibras = localStorage.getItem('storageLibras');
- 
+  let openLibrasButton:HTMLElement;
+
+  for (let index = 0; index < document.getElementsByTagName('IMG').length; index++) {
+    const element: HTMLElement = document.getElementsByTagName('IMG')[ index ] as HTMLElement;
+    if (element.getAttribute('class') === 'access-button') {
+     openLibrasButton =  element
+    }
+  }
+
   const setLibras = () => {
 
     if (storageLibras == '1') {
       localStorage.setItem('storageLibras', JSON.stringify(2))
-
-      for (let index = 0; index < document.getElementsByTagName('IMG').length; index++) {
-        const element: HTMLElement = document.getElementsByTagName('IMG')[ index ] as HTMLElement;
-        if (element.getAttribute('class') === 'access-button') {
-          element.click()
-        }
-      }
+      openLibrasButton.click()
 
       setTimeout(() =>
         document.getElementsByClassName('vpw-settings-btn-close')[ 0 ]
           .addEventListener('click', () =>
             localStorage.setItem('storageLibras', JSON.stringify(1)))
-        , 15000);
+        , 10000);
 
     } else if (storageLibras == '2') {
       localStorage.setItem('storageLibras', JSON.stringify(1))
